a4f31c8c5d2d944f154cd922fb310f796d4edaf4
Virginie Beernaert <vbeernaert@yahoo.fr>
2019-07-02 15:09:39 +0200

    add data

---
+++ seeds2.js
@@ -0,0 +1,28 @@
+(module.exports = [
+  { img: "/media/img1.jpg" },
+  { img: "/media/img2.jpg" },
+  { img: "/media/IMG_3916.jpg" },
+  { img: "/media/IMG_3914.jpg" },
+  { img: "/media/img3.jpg" },
+  { img: "/media/img4.jpg" },
+  { img: "/media/img5.jpg" },
+  { img: "/media/img6.jpg" },
+  { img: "/media/img7.jpg" },
+  { img: "/media/img8.jpg" }
+]),
+  [
+    { sentence: "Take a coffee break with colleagues" },
+    { sentence: "Add more greenery to your home" },
+    { sentence: "Turn off your phone before sleeping at night" },
+    { sentence: "Take an evening walk without your phone" },
+    { sentence: "Read a book and enjoy a hot beverage" },
+    { sentence: "Open the curtains for natural light" },
+    {
+      sentence: "Light candles and write a list of things you're thankful for"
+    },
+    { sentence: "Ride a bike and enjoy nature" },
+    { sentence: "Grab a cosy blanket and watch a movie " },
+    { sentence: "Sit outside and watch the sunrise or sunset" },
+    { sentence: "Forget life's worries" },
+    { sentence: "Be present in the moment" }
+  ];
